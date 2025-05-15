bdbc0ca32b1dd5471243bcc5e9fa9e4cca1d77ca
MILLENA  OLIVEIRA <millena.s.brandao@gmail.com>
2025-05-15 16:29:08 -0300

    Documentação
    Foi colocado uma documentação do que cada função faz.

--- utils.h
+++ utils.h
@@ -2,8 +2,32 @@
 #define UTILS_H
 
 // Cabeçalhos
+
+/**
+ * @brief Cria uma quebra de linha no terminal.
+ */
 void quebrarLinha();
+
+/**
+ * @brief Limpa a tela do terminal.
+ */
 void limparTela();
+
+/**
+ * @brief Pausa a tela até que o usuário pressione Enter.
+ */
 void pausarTela();
+
+/**
+ * @brief Imprime uma linha horizontal com o caractere especificado.
+ * @param caractere Caractere a ser repetido.
+ */
+void desenharLinhaHorizotal(char caractere);
+
+/**
+ * @brief Centraliza e exibe um texto no terminal.
+ * @param texto Ponteiro para string a ser exibida.
+ */
+void mostrarTextoCentralizado(const char *texto);
 
 #endif
