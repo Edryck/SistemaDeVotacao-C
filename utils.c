5c90b50e4be14fee97065a7666e079d52d26edf8
MILLENA  OLIVEIRA <millena.s.brandao@gmail.com>
2025-05-15 16:28:17 -0300

    Documentação
    Foi colocado uma documentação do que cada função faz e um comentário (quando necessário).
    
    Foi incluída uma função que centraliza o texto e uma outra função que desenha uma linha horizontal

--- utils.c
+++ utils.c
@@ -3,16 +3,62 @@
 #include "utils.h"
 #include "ui_utils.h"
 
-// Cabeçalhos
+
+/**
+ * @brief Imprime uma quebra de linha.
+ */
 void quebrarLinha() {
+    printf("\n"); //Cabeçalhos
+}
+
+/**
+ * @brief Limpa a tela usando comandos do sistema operacional.
+ */
+void limparTela() {
+    #ifdef _WIN32
+        system("cls");  // Windows
+    #else
+        system("clear");  // Linux/macOS
+    #endif
+}
+
+/**
+ * @brief Pausa a execução do programa até o usuário pressionar Enter.
+ */
+void pausarTela() {
+    animarCarregamento("Pressione ENTER para continuar", 500);
+    getchar(); // limpa o buffer
+    getchar(); // espera input;
+}
+
+/**
+ * @brief Imprime uma linha horizontal com um caractere repetido.
+ * @param caractere Caractere que será usado.
+ */
+void desenharLinhaHorizontal(char caractere) {
+    for (int i = 0; i < LARGURA_TERMINAL; i++) {
+        printf("%c", caractere);
+    }
     printf("\n");
 }
 
-void limparTela() {
-    system("cls");
+/**
+ * @brief Imprime uma quebra de linha.
+ */
+void quebrarLinha() {
+    printf("\n"); // Cabeçalhos
 }
 
-void pausarTela() {
-    animarCarregamento("Pressione ENTER para continuar", 500);
-    getchar(); // Aguarda o pressionamento de ENTER
+/**
+ * @brief Exibe texto centralizado no terminal.
+ * @param texto Ponteiro para a string a ser exibida.
+ */
+void mostrarTextoCentralizado(const char *texto) {
+    int tamanhoTexto = strlen(texto);
+    int espacos = (LARGURA_TERMINAL - tamanhoTexto) / 2;
+
+    for (int i = 0; i < espacos; i++) {
+        printf(" ");
+    }
+    printf("%s\n", texto);
 }
