97d6394b61c058b53e06cef9749c5cdaec5624f0
Edryck Nascimento <edryckfreitas@gmail.com>
2025-06-12 10:02:52 -0300

    Funções do eleitor e validação do cpf corrigida... Eu acho

--- eleitor.c
+++ eleitor.c
@@ -1,52 +1,14 @@
-
 #include "eleitor.h"
 
+#include "utils.h"
 
-
-void menuEleitor() {
-    limparTela();
-
-    cabecalho("MENU ELEITOR"); // cabecalho
-    // Exibição das informações do eleitor
-    // Adicionar aqui a lógica para exibir as informações do eleitor
-
-    printf("1 - Ver candidatos\n");
-    printf("2 - Votar\n");
-    printf("3 - Sair\n");
-    printf("Escolha uma opcao: ");
-
-    int opcao;
-    scanf("%d", &opcao);
-    switch(opcao) {
-        case 1: // Chama a função para ver candidatos
-            printf("Em desenvolvimento!\n");
-            pausarTelaInt();
-            menuEleitor();
-            break;
-        case 2: // Chama a função para votar
-            printf("Em desenvolvimento!\n");
-            pausarTelaInt();
-            menuEleitor();
-            break;
-        case 3: // Sair do menu eleitor
-            printf("Saindo...\n");
-            pausarTelaInt();
-            menuPrincipal(); // Retorna ao menu principal
-            break;
-        default:
-            printf("Opcao invalida! Tente novamente.");
-            pausarTelaInt();
-            menuEleitor(); // Chama o menu novamente
-            break;
-    }
-}
-int cpfJaCadastrado(char* cpf) {
-    FILE* arquivo = fopen(ARQUIVO_ELEITORES, "r");
+int validarCpf (Eleitor eleitores[], int totalEleitores, const char* cpf) {
+    FILE* arquivo = fopen(ELEITORES, "r");
     if (arquivo == NULL) {
-        return 0; // Arquivo não existe
+        return 0;
     }
 
-    char linha[256];
+    char linha[tam];
     
     while (fgets(linha, sizeof(linha), arquivo) != NULL) {
         char* token = strtok(linha, "|");
@@ -55,9 +17,101 @@ int cpfJaCadastrado(char* cpf) {
         if (token != NULL && strcmp(token, cpf) == 0) {
             fclose(arquivo);
             return 1; 
-        } else {
-            fclose(arquivo);
-            return 0; 
         }
     }
+    return 0;
+}
+
+void cadastroEleitor(Eleitor listaDeEleitores[], int *totalEleitores) {
+    if(fase == FASE_CADASTRO) {
+        if (*totalEleitores >= MAX_ELEITORES) {
+            printf("ERRO: Limite maximo de eleitores atingido!\n");
+            return;
+        }
+
+        Eleitor novoEleitor;
+
+        printf("Informe os seguintes dados: \n");
+        printf("Nome completo: ");
+        scanf(" %[^\n]", novoEleitor.nome);
+        printf("CPF (somente numeros): ");
+        scanf("%11s", novoEleitor.cpf); 
+        
+        //chama funçao pra verificar se ja existe o cpf cadastrado
+        if (validarCpf(listaDeEleitores, *totalEleitores, novoEleitor.cpf)) {
+            printf("\nErro: CPF ja cadastrado no sistema.\n");
+            pausarTelaInt();
+            return;
+        }
+        
+        printf("Idade: ");
+        scanf("%d", &novoEleitor.idade);
+        
+        if (novoEleitor.idade < 18) {
+            printf("\nErro: Eleitor deve ter no minimo 18 anos para se cadastrar.\n");
+            pausarTela(3000);
+            return;
+        }
+        
+        FILE* arquivo = fopen(ELEITORES, "a");
+            if (arquivo == NULL) {
+                printf("\nErro ao abrir arquivo de eleitores!\n");
+                pausarTela(3000);
+                return;
+            }
+            
+            novoEleitor.votou = 0;
+
+            fprintf(arquivo, "%s|%s|%d|%d\n", 
+                    novoEleitor.nome, 
+                    novoEleitor.cpf, 
+                    novoEleitor.idade,
+                    novoEleitor.votou); // Como acabou de cadastrar o eleitor, então inicializa com 0
+
+        fclose(arquivo);
+
+        registrarLog("Novo eleitor cadastrado no sistema.");
+    } else {
+        printf("A fase de cadastro terminou! Nao sera possivel realizar nenhum cadastro.");
+        pausarTelaInt();
+    }
+}
+
+void votar(Candidato candidatos[], int totalCandidatos, Eleitor *eleitorLogado) {
+    if (fase != FASE_VOTACAO) {
+        printf("A fase de votacao nao esta ativa!\n");
+        pausarTelaInt();
+        return;
+    }
+    
+    if (eleitorLogado->votou == 1) {
+        printf("Voce ja votou! Nao pode votar novamente!\n");
+        registrarLog("Tentativa de realizar mais de um voto!");
+        pausarTelaInt();
+        return;
+    }
+
+    int numeroVoto;
+    printf("Digite o numero do seu candidato: ");
+    scanf("%d", &numeroVoto);
+
+    int indiceCandidato = -1;
+    for (int i = 0; i < totalCandidatos; i++) {
+        if (candidatos[i]->numero == numeroVoto) {
+            indiceCandidato = i;
+            break;
+        }
+    }
+
+    if (indiceCandidato != -1) {
+        candidatos[indiceCandidato]->votos++;
+        eleitorLogado->votou = 1;
+        printf("Voto realizado com sucesso!\n");
+        registrarLog("Voto registrado com sucesso.");
+    } else {
+        printf("Candidato nao encontrado. Seu voto sera computado como NULO.\n");
+        // Lógica do voto nulo
+    }
+    
+    pausarTelaInt();
 }
--- eleitor.h
+++ eleitor.h
@@ -1,25 +1,51 @@
-#ifndef ELEITOR_H  
+/**
+ * @file eleitor.
+ * @brief Funções das opções do eleitor
+ * 
+ * Este módulo fornece as funções de validação, cadastro e de voto do eleitor.
+ */
+#ifndef ELEITOR_H
 #define ELEITOR_H
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+    #include <string.h>
 
-#include "menu.h"
-#include "log.h"
-#include "utils.h"
+    #include "dados.h"
 
-#define ARQUIVO_ELEITORES "eleitores.txt"
+    #define tam 256
+    #define ELEITORES "eleitores.txt"
 
-typedef struct {
-    char nome[100];
-    char cpf[15];
-    int idade;
-    char tituloEleitor[20];
-} Eleitor;
+    /**
+     * @brief Função para verificar se o eleitor já foi cadastrado
+     * 
+     * Esta função verifica se o eleitor já foi cadastrado percorrendo
+     * um arquivo onde os dados dos eleitores já cadastrados estão
+     * armazenados. 
+     * 
+     * @param cpf "Chave-primária" do eleitor, também o que será buscado
+     * no arquivo.
+     * 
+     * @return Retorna 1 se o eleitor já foi cadastrado.
+     * @return Retorna 0 se o eleitor não foi cadastrado.
+     */
+    int validarCpf (Eleitor eleitores[], int totalEleitores, const char* cpf);
 
-// Cabeçalhos
-void menuEleitor();
-int cpfJaCadastrado( char* cpf);
+    /**
+     * @brief Função para cadastro de eleitores
+     * 
+     * Está função cadastra o eleitor no sistema 
+     */
+    void cadastroEleitor(Eleitor listaDeEleitores[], int *totalEleitores);
+    
+    /**
+     * @brief Função de votar.
+     * 
+     * Está função realiza o voto do eleitor, ela realiza primeiramente se está na 
+     * fase de votos, se estiver, ela verifica se usuário já voltou, caso não tenha
+     * votado ela incrementa no contador de votos do candidato.
+     * @param candidato Candidato que o eleitor escolheu votar.
+     * @param eleitor Eleitor que está logado.
+     */
+    void votar(Candidato candidatos[], int totalCandidatos, Eleitor *eleitorLogado);
+   
 
 #endif
